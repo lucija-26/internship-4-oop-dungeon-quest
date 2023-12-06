e66933dd0c15902bc5120b7abefe8872ebaba36a
lucija-26 <Lucija.Jurkovic.02@fesb.hr>
2023-12-06 22:11:52 +0100

    made goblin class

---
+++ DungeonQuest.Domain/Repositories/Monster/Goblin.cs
@@ -0,0 +1,28 @@
+﻿
+
+namespace DungeonQuest.Domain.Repositories.Monster
+{
+    using DungeonQuest.Domain.Repositories.Hero;
+
+    public class Goblin : Monster
+    {
+        Random random = new Random();
+        public Goblin(string name, double hp, double damage, double xp, bool isStunned) : base(hp, damage, xp, isStunned)
+        {
+            Name = name;
+            HealthPoints = random.Next(5, 25);
+            Damage = random.Next(5, 25);
+            ExperienceValue = random.Next(10, 25);
+            IsStunned = isStunned;
+        }
+
+        public override void Attack(Hero hero)
+        {
+            base.Attack(hero);
+        }
+        public override void TakeDamage(double damage)
+        {
+            base.TakeDamage(damage);
+        }
+    }
+}
