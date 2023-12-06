9a4dd71ff7167dda92a7131ec6b13ef1226432fe
lucija-26 <Lucija.Jurkovic.02@fesb.hr>
2023-12-06 22:06:36 +0100

    made gladiator class

---
+++ DungeonQuest.Domain/Repositories/Hero/Gladiator.cs
@@ -0,0 +1,39 @@
+﻿
+
+namespace DungeonQuest.Domain.Repositories.Hero
+{
+    using DungeonQuest.Domain.Repositories.Monster;
+    using System.Threading;
+    using static System.Net.Mime.MediaTypeNames;
+
+    public class Gladiator : Hero
+    {
+        public bool IsInRage { get; private set; }
+
+        public Gladiator(string name, double initialHealthPoints, double damage, double experience) : base(name, initialHealthPoints)
+        {
+            Damage = damage;
+            Experience = experience;
+            IsInRage = false;
+        }
+
+        public void AttackWithRage(Monster monster)
+        {
+            if (IsInRage)
+            {
+                double damageDealt = Damage * 2;
+                double rageCost = HealthPoints * 0.15;
+                HealthPoints -= rageCost;
+                IsInRage = false;
+                monster.TakeDamage(damageDealt);
+                return;
+            }
+            Attack(monster);
+        }
+
+        public void ActivateRage()
+        {
+            IsInRage = true;
+        }
+    }
+}
