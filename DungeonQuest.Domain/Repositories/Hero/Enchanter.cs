2b820c4c5d44186c6f0776cd7307bdc9acf78255
lucija-26 <Lucija.Jurkovic.02@fesb.hr>
2023-12-06 22:07:56 +0100

    made enchanter class

---
+++ DungeonQuest.Domain/Repositories/Hero/Enchanter.cs
@@ -0,0 +1,55 @@
+﻿using DungeonQuest.Domain.Repositories.Hero;
+using DungeonQuest.Domain.Repositories.Monster;
+
+
+public class Enchanter : Hero
+{
+    public double MaxMana { get; private set; }
+    public double Mana { get; set; }
+    public bool IsRevived { get; private set; }
+
+    public Enchanter(string name, double initialHealthPoints, double damage, double experience) : base(name, initialHealthPoints)
+    {
+        Damage = damage;
+        Experience = experience;
+        Mana = 0;
+        MaxMana = 0;
+        IsRevived = false;
+    }
+
+    public void AttackWithMana(Monster monster)
+    {
+        if (Mana > 0)
+        {
+            double damageDealt = Damage;
+            Mana--;
+            monster.TakeDamage(damageDealt);
+            return;
+        }
+        Attack(monster);
+    }
+
+    public void RestoreMana()
+    {
+        Mana = MaxMana;
+    }
+
+    public void RestoreHealthWithMana(double manaCost)
+    {
+        if (Mana >= manaCost)
+        {
+            HealthPoints = MaxHealthPoints;
+            Mana -= manaCost;
+        }
+    }
+
+
+    public void Revive()
+    {
+        if (!IsRevived)
+        {
+            IsRevived = true;
+            HealthPoints = MaxHealthPoints;
+        }
+    }
+}
