766ff7eba215e2633594d2de8d62fa5ed147ef27
lucija-26 <Lucija.Jurkovic.02@fesb.hr>
2023-12-06 22:23:51 +0100

    made a switch-case where user picks hero

---
+++ DungeonQuest.Domain/Repositories/Game/Round.cs
@@ -0,0 +1,62 @@
+﻿namespace DungeonQuest.Domain.Repositories.Round
+{
+    using DungeonQuest.Domain.Repositories.Monster;
+    using DungeonQuest.Domain.Repositories.Hero;
+
+    public class Round
+    {
+        public string HeroAction { get; }
+        public string MonsterAction { get; }
+        public int RoundNumber { get; set; }
+
+        public Round(string heroAction, string monsterAction, int roundNumber)
+        {
+            HeroAction = heroAction;
+            MonsterAction = monsterAction;
+            RoundNumber = roundNumber;
+        }
+
+        private bool BeatingMove()
+        {
+            if (MonsterAction == "Counter" && HeroAction == "Direct")
+                return true;
+            else if (MonsterAction == "Direct" && HeroAction == "Side")
+                return true;
+            else if (MonsterAction == "Side" && HeroAction == "Counter")
+                return true;
+            return false;
+        }
+
+        public bool HeroWins(Hero hero, Monster monster)
+        {
+
+            if (HeroAction == MonsterAction)
+            {
+                Console.WriteLine("Both sides picked the same action");
+                return false;
+            }
+            else if (BeatingMove())
+            {
+                hero.Attack(monster);
+                if (monster.HealthPoints <= 0)
+                {
+                    Console.WriteLine("Hero wins this round!");
+                    hero.LevelUp(10, 10, 10);
+                    hero.victories++;
+                    return true;
+                }
+                return false;
+
+            }
+            else
+            {
+                monster.Attack(hero);
+
+                if (hero.HealthPoints <= 0)
+                    Console.WriteLine("Monster wins this round!");
+                return false;
+
+            }
+        }
+    }
+}
---
+++ DungeonQuest.Presentation/program.cs
@@ -0,0 +1,38 @@
+﻿using DungeonQuest.Domain.Repositories.Hero;
+using DungeonQuest.Domain.Repositories.Monster;
+using DungeonQuest.Domain.Repositories.Game;
+
+
+bool status = true;
+while (status)
+{
+    Console.WriteLine("Press enter to play or 0 to exit");
+    var choice = Console.ReadLine();
+    if (choice == "0")
+    {
+        Console.WriteLine("Thanks for playing!");
+        break;
+    }
+    Console.Clear();
+    Console.WriteLine("Choose your hero: ");
+    HeroOptions();
+    int heroChoice = GetNumberFromUser();
+    switch (heroChoice)
+    {
+        case 1:
+            GladiatorChoice();
+            break;
+        case 2:
+            EnchanterChoice();
+            break;
+        case 3:
+            MarksmanChoice();
+            break;
+        case 0:
+            status = false;
+            break;
+        default:
+            Console.WriteLine("Please pick a valid option.");
+            break;
+    }
+}
